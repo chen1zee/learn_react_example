a67e55f800b9fcfa1cee8cb002fc74386fdfa050
chen1zee <chen2zee1@gmail.com>
2019-08-28 18:00:51 +0800

    [ing]08_28_02

--- src/learn/optimizingPerform/optimizingPerform.js
+++ src/learn/optimizingPerform/optimizingPerform.js
@@ -3,9 +3,7 @@ import React from "react"
 class CounterButton extends React.PureComponent {
   constructor(props) {
     super(props)
-    // TODO state内 key==普通类型时， 如 number
     // 调用 setState 不能引起 componentUpdate
-    // TODO 看源码处理
     this.state = {
       aaa: {
         count: 1
